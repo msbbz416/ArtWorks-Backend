3ceb1309db5de74f97c4dce643cd7be40f2d3d84
Megan Mcgowan <meganmcgowan1205962@gmail.com>
2020-07-01 20:52:14 -0400

    added database connection.js

---
+++ Test/TestMocha.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+// Describe our tests
+describe('testing mocha', function(){
+
+  // Create tests
+  it('adds numbers that equal to 5', function(){
+    assert(2 + 3 === 5);
+  });
+
+});
