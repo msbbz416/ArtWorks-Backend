2e3cf18770f632da7291a01d974dd6c48ecdfa99
Elliana Madsen <elliana.a.madsen@gmail.com>
2020-07-10 17:50:12 -0500

    Elliana's first commit for backend

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser=require("body-parser"); 
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const ArtWorks = require ('./models/artists');
+
+const mongoose = require('mongoose'); 
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0.yy25u.gcp.mongodb.net/charityWater?retryWrites=true&w=majority' , { useNewUrlParser: true });; 
+
+var db=mongoose.connection; 
+var Schema = mongoose.Schema;
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+
 var app = express();
 
+app.use(bodyParser.json()); 
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+  
+  
+console.log("server listening at port 3000"); 
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var artists = require('../models/artists');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signup', function (req, res) {
+  var data = new artists({
+    Name: req.body.name,
+    Email: req.body.email,
+    Portfolio: req.body.portfolio,
+    Photo: req.body.photo
+  });
+  console.log(data);
+  data.save(function (err) {
+    if (err) throw err;
+    console.log("Record inserted Successfully");
+    res.json({
+      message: "Record inserted Successfully",
+      status: 200
+    })
+  });
+})
+
 
 module.exports = router;
